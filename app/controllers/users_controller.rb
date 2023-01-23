7cf81f6c07b40e1253db0fd0ea2f974966c442e2
Jonathan <jonitoadriano@gmail.com>
2023-01-23 15:51:40 -0600

    pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where(role_id: 1)
+    @users = User.include(role_id: 1)
+    # @users = User.where(role_id: 1).order(id: :ASC).limit(10).offset(offset)
     render template: "users/index"
   end
 end
